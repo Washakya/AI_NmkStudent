62406e356504563aa1c3a00538a5b572482f17e6
Washakya <86008644+Washakya@users.noreply.github.com>
2022-10-27 00:26:36 +0900

    Update Run.py

--- Run.py
+++ Run.py
@@ -4,3 +4,7 @@ API_KEY = os.environ.get("API_KEY")
 API_KEY_SECRET = os.environ.get("API_KEY_SECRET")
 ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
+
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
