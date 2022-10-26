298ac3c48278607387c893d9e77528ee9978eb5b
Washakya <86008644+Washakya@users.noreply.github.com>
2022-10-27 00:11:20 +0900

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,4 +1,6 @@
 #でばっぐだよ
 import os
 API_KEY = os.environ.get("API_KEY")
-print(type(API_KEY))
+API_KEY_SECRET = os.environ.get("API_KEY_SECRET")
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
