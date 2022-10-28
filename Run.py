6ee01cc29d9eea9b8183502b7752e5dade9a4197
Washakya <86008644+Washakya@users.noreply.github.com>
2022-10-28 16:48:30 +0900

    Update Run.py

--- Run.py
+++ Run.py
@@ -2,7 +2,7 @@
 import os
 import tweepy
 
-API_KEY = os.environ.get("API_KEY")
+API_KEY = os.environ.get("API_KEY ")
 API_KEY_SECRET = os.environ.get("API_KEY_SECRET")
 ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
