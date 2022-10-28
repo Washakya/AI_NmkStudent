51c3ce53d562453044f9eba0170be724a95e12b1
Washakya <86008644+Washakya@users.noreply.github.com>
2022-10-28 17:02:54 +0900

    Update Run.py

--- Run.py
+++ Run.py
@@ -6,11 +6,9 @@ API_KEY = os.environ.get("API_KEY")
 API_KEY_SECRET = os.environ.get("API_KEY_SECRET")
 ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
-"""
+
 auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
-"""
-print(len(API_KEY))
 
-#api.update_status("環境変数てすと")
+api.update_status("環境変数てすと")
